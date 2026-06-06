2d3dbf292836c8557a9bb88c4a4c61b936785b62
noob <papakisprits@gmail.com>
2026-06-06 10:16:35 +0300

    new features ytay

--- test.c
+++ test.c
@@ -5,6 +5,7 @@
 
   struct mpd_connection *mpd_conn = NULL;
   GtkTextBuffer *buffer = NULL;
+  GtkWidget *progress = NULL;
 
 
   static void errorchecker(struct mpd_connection *conn) {
@@ -16,6 +17,21 @@
 
   static gboolean songdetection(gpointer data) {
       if (mpd_conn == NULL || buffer == NULL) return TRUE;
+
+      struct mpd_status *status = mpd_run_status(mpd_conn);
+        if (status != NULL) {
+        unsigned elapsed = mpd_status_get_elapsed_time(status);
+        unsigned total = mpd_status_get_total_time(status);
+
+        if (total > 0) {
+            // im bad at mathhhhhh
+            double fraction = (double)elapsed / (double)total;
+            gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progress), fraction);
+        } else {
+            gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progress), 0.0);
+        }
+        mpd_status_free(status);
+    }
 
       struct mpd_song *song = mpd_run_current_song(mpd_conn);
 
@@ -46,7 +62,6 @@
       if (mpd_conn) {
           mpd_run_next(mpd_conn);
           errorchecker(mpd_conn);
-          g_timeout_add(1000, (GSourceFunc)songdetection, NULL);
       }
   }
 
@@ -54,11 +69,36 @@
       if (mpd_conn) {
           mpd_run_previous(mpd_conn);
           errorchecker(mpd_conn);
-          g_timeout_add(1000, (GSourceFunc)songdetection, NULL);
       }
   }
 
+    static void add5secButtonClicked(GtkWidget *widget, gpointer data) {
+      if (mpd_conn) {
+          struct mpd_status *status = mpd_run_status(mpd_conn);
+          if (status != NULL) {
+              int current_pos = mpd_status_get_song_pos(status);
+              unsigned elapsed = mpd_status_get_elapsed_time(status);
+              unsigned new_time = elapsed + 5;
+              mpd_run_seek_pos(mpd_conn, current_pos, new_time);
+              mpd_status_free(status);
+          }
+          errorchecker(mpd_conn);
+      }
+  }
 
+  static void remove5secButtonClicked(GtkWidget *widget, gpointer data) {
+      if (mpd_conn) {
+          struct mpd_status *status = mpd_run_status(mpd_conn);
+          if (status != NULL) {
+              int current_pos = mpd_status_get_song_pos(status);
+              unsigned elapsed = mpd_status_get_elapsed_time(status);
+              unsigned new_time = elapsed - 5;
+              mpd_run_seek_pos(mpd_conn, current_pos, new_time);
+              mpd_status_free(status);
+          }
+          errorchecker(mpd_conn);
+      }
+  }
 
   static void pauseButtonClicked(GtkWidget *widget, gpointer data) {
       if (mpd_conn) {
@@ -123,6 +163,10 @@
       GtkWidget *button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
       gtk_box_pack_start(GTK_BOX(main_box), button_box, FALSE, FALSE, 5);
 
+      GtkWidget *button_box2 = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+      gtk_box_pack_start(GTK_BOX(main_box), button_box2, FALSE, FALSE, 5);
+      gtk_button_box_set_layout(GTK_BUTTON_BOX(button_box2), GTK_BUTTONBOX_CENTER);
+
       GtkWidget *text_songinfo = gtk_text_view_new();
       gtk_text_view_set_editable(GTK_TEXT_VIEW(text_songinfo), FALSE);
 
@@ -134,23 +178,33 @@
 
       gtk_container_add(GTK_CONTAINER(button_box), text_songinfo);
 
+      progress = gtk_progress_bar_new();
+
+      gtk_box_pack_start(GTK_BOX(main_box), progress, FALSE, FALSE, 5);
+
       GtkWidget *btn_play = gtk_button_new_with_label("Play");
       GtkWidget *btn_pause = gtk_button_new_with_label("Pause");
       GtkWidget *btn_stop = gtk_button_new_with_label("Stop");
       GtkWidget *btn_next = gtk_button_new_with_label("Next");
       GtkWidget *btn_previous = gtk_button_new_with_label("Previous");
+      GtkWidget *btn_add5sec = gtk_button_new_with_label("+5s");
+      GtkWidget *btn_remove5sec = gtk_button_new_with_label("-5s");
 
       gtk_container_add(GTK_CONTAINER(button_box), btn_play);
       gtk_container_add(GTK_CONTAINER(button_box), btn_pause);
       gtk_container_add(GTK_CONTAINER(button_box), btn_stop);
       gtk_container_add(GTK_CONTAINER(button_box), btn_next);
       gtk_container_add(GTK_CONTAINER(button_box), btn_previous);
+      gtk_container_add(GTK_CONTAINER(button_box2), btn_add5sec);
+      gtk_container_add(GTK_CONTAINER(button_box2), btn_remove5sec);
 
       g_signal_connect(btn_play, "clicked", G_CALLBACK(playButtonClicked), NULL);
       g_signal_connect(btn_pause, "clicked", G_CALLBACK(pauseButtonClicked), NULL);
       g_signal_connect(btn_stop, "clicked", G_CALLBACK(stopButtonClicked), NULL);
       g_signal_connect(btn_next, "clicked", G_CALLBACK(nextButtonClicked), NULL);
       g_signal_connect(btn_previous, "clicked", G_CALLBACK(previousButtonClicked), NULL);
+        g_signal_connect(btn_add5sec, "clicked", G_CALLBACK(add5secButtonClicked), NULL);
+      g_signal_connect(btn_remove5sec, "clicked", G_CALLBACK(remove5secButtonClicked), NULL);
 
       g_timeout_add(1000, (GSourceFunc)songdetection, NULL);
 
